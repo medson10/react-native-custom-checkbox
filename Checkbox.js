93759234312aa7f40ca45227348027854d6ceeff
Carolina Aguilar <caroagse@gmail.com>
2016-05-10 19:17:52 -0600

    chg: dev: Store properties in state in order to properly customize different instances of Checkbox in a same component.

--- Checkbox.js
+++ Checkbox.js
@@ -2,42 +2,40 @@ import React, { Component } from 'react';
 import { Text, TouchableHighlight, View } from 'react-native';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
+import _ from 'underscore';
 
-var DEFAULT_BACKGROUND_COLOR = '#FFF';
-var DEFAULT_BORDER_RADIUS = 0;
-var DEFAULT_BORDER_WIDTH = 2;
-var DEFAULT_COLOR = '#000';
-var DEFAULT_MARGIN = 2;
-var DEFAULT_PADDING = 0;
-var DEFAULT_SIZE = 20;
-
-var BACKGROUND_COLOR = DEFAULT_BACKGROUND_COLOR;
-var BORDER_RADIUS = DEFAULT_BORDER_RADIUS;
-var BORDER_WIDTH = DEFAULT_BORDER_WIDTH;
-var COLOR = DEFAULT_COLOR;
-var MARGIN = DEFAULT_MARGIN;
-var PADDING = DEFAULT_PADDING;
-var SIZE = DEFAULT_SIZE;
+var BACKGROUND_COLOR, BORDER_RADIUS, BORDER_WIDTH, COLOR, MARGIN, PADDING, SIZE;
 
 class Checkbox extends Component {
     constructor(props) {
         super(props);
-        this.state = { checked: false };
+        this.state = {
+            backgroundColor: '#FFF',
+            borderRadius: 0,
+            borderWidth: 2,
+            checked: false,
+            color: '#000',
+            margin: 2,
+            padding: 0,
+            name: '',
+            onChange: null,
+            size: 20
+        };
     }
 
     componentDidMount() {
-        BACKGROUND_COLOR = this.props.style.backgroundColor || DEFAULT_BACKGROUND_COLOR;
-        BORDER_RADIUS = this.props.style.borderRadius || DEFAULT_BORDER_RADIUS;
-        BORDER_WIDTH = this.props.style.borderWidth || DEFAULT_BORDER_WIDTH;
-        MARGIN = this.props.style.margin || DEFAULT_MARGIN;
-        PADDING = this.props.style.padding || DEFAULT_PADDING;
-        COLOR = this.props.style.color || DEFAULT_COLOR;
-        SIZE = this.props.size || DEFAULT_SIZE;
-
-        this.setState({ checked: this.props.checked });
+        this.setState(_.extend(this.props.style, _.omit(this.props, 'style')))
     }
 
     render() {
+        BACKGROUND_COLOR = this.state.backgroundColor;
+        BORDER_RADIUS = this.state.borderRadius;
+        BORDER_WIDTH = this.state.borderWidth;
+        MARGIN = this.state.margin;
+        PADDING = this.state.padding;
+        COLOR = this.state.color;
+        SIZE = this.state.size;
+
         return (
             <TouchableHighlight underlayColor='transparent'
                 onPress={() => { this._toggleCheck() }}
